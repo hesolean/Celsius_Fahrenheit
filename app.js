9a35384741acb9e262e02b7618db1880f5282018
unknown <lndubourg@gmail.com>
2023-08-31 10:16:21 +0200

    2 champs pour entrer la valeur en celsius ou en fahrenheit

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
+const scaleNames = {
+    c: 'celsius',
+    f: 'hrenheit'
+}
+
 function BoilerVerdict({celsius}) {
     const message = celsius>100 ? "L'eau bout" : "L'eau ne bout pas"
     return <p className="alert">{message}</p>
+}
+
+class TemperatureInput extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            temperature: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            temperature: e.target.value
+        })
+    }
+    
+    render(){
+        const {temperature} = this.state
+        const {unite} = scaleNames[this.props.scale]
+        return <div className="form-group">
+                <label htmlFor={unite}>Donner une température en {unite} : </label>
+                <input 
+                    type="text" 
+                    className="form-control"
+                    id={unite} 
+                    name={unite}
+                    value={temperature}
+                    onChange={this.handleChange}
+                />
+            </div>
+    }
 }
 
 class Calculator extends React.Component {
@@ -9,40 +46,17 @@ class Calculator extends React.Component {
         this.state = {
             temperature: ""
         }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-
     }
 
-    handleChange(e) {
-        const celsius = e.target.value
-        this.setState({
-            temperature: celsius
-        })
-    }
-
-    handleSubmit(e) {
-        e.preventDefault()
-        const data = JSON.stringify(this.state)
-        return data
-    }
     render() {
         // on destructure pour ne plus à avoir à ajouter à chaque fois this.state.temperature
         const {temperature} = this.state
-        return <form className="container" onSubmit={this.handleSubmit}>
-            <label htmlFor="celsius">Donner une température en Celsius : </label>
-            <input 
-                type="text" 
-                className="form-control"
-                id="celsius" 
-                name="celsius"
-                value={temperature}
-                onChange={this.handleChange}
-            />
-            {/* {JSON.stringify(this.state)} */}
-            <BoilerVerdict celsius={parseFloat(temperature)}/>
-        </form>
-        
+        return <div className='container'>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+                {/* {JSON.stringify(this.state)} */}
+                <BoilerVerdict celsius={parseFloat(temperature)}/>
+            </div>
     }
 }
 
